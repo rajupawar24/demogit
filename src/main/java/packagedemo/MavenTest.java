03d3aa017e13c69391eae8fd1f476e55731861e3
Raju pawar <rajupawar16@gmail.com>
2019-08-14 10:36:02 +0530

    first commit

---
+++ src/main/java/packagedemo/MavenTest.java
@@ -0,0 +1,17 @@
+package packagedemo;
+
+import org.testng.annotations.Test;
+
+public class MavenTest 
+{
+	@Test
+	public void maventestA()
+	{
+		System.out.println("Welcome to maven");
+	}
+	@Test
+	public void maventestB()
+	{
+		System.out.println("Test case 2 execution.");
+	}
+}
